48f7da721acfba4815885b3a39b1968b9a97c801
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-04-02 01:02:07 +0400

    encapculiton User

---
+++ User Task/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace User_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("mAhir   ", "   mUsayev", "mahir123");
+            user.ChangePassword("mahir123", "musayev123");
+
+            Console.WriteLine(user.UserName);
+        }
+    }
+}
